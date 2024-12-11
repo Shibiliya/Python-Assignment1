74d7f38472670baba568db375ae9167c44a4864f
Shibiliya <shibiliya.shibi@gmail.com>
2024-12-11 20:11:27 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,56 @@
+#EXERCISE 1
+print("Shibiliya")
+print("BNd42")
+print("shibiliya.shibi@gmail.com")
+#EXERCISE 2
+print("Shibiliya\nBNd42\nshibiliya.shibi@gmail.com")
+#EXERCISE 3
+num1=14
+num2=7
+print(f"{num1}+{num2}={num1+num2}")
+print(f"{num1}*{num2}={num1*num2}")
+print(f"{num1}-{num2}={num1-num2}")
+print(f"{num1}/{num2}={num1/num2}")
+#EXERCISE 4
+print("1\n2\n3\n4\n5")
+#EXERCISE 5
+print('"SDK"stands for "Software Development Kit",whereas\n"IDE"stands for "Integrated Development Environment".')
+# EXERCISE 6
+print ( "python is an \"awesome \"language." )
+print("python\n\t2023")
+print('I\'m from Entri.\b')
+print("\65")
+print("\x65")
+print("Entri","2023",sep="\n")
+print("Entri","2023",sep="\b")
+print("Entri","2023",sep="*",end="\b\b\b\b")
+#EXERCISE 7
+num=23
+textnum="57"
+decimal=98.3
+#printing types
+print(type(num))
+print(type(textnum))
+print(type(decimal))
+#calculating the sum
+total=num+int(textnum)+decimal
+print(f"The sum is:{total}")
+print(f"The datatype of the sum is:{type(total)}")
+#EXERCISE 8
+days_in_year=365
+hours_in_day=24
+minutes_in_hour=60
+total_minutes=days_in_year*hours_in_day*minutes_in_hour
+print("This code calculates the total number of minutes in a year.")
+print(f"Total minutes in a year:{total_minutes}")
+#EXERCISE 9
+name=input("Please enter your name:")
+print(f"Hi {name},welcome to python programming:")
+#EXERCISE 10
+#PoundsToDollars.py
+pounds=float(input("Please enter amount in pounds:€"))
+conversion_rate=1.3
+dollars=pounds*conversion_rate
+print(f"€{pounds} are ${dollars}")
+
+
